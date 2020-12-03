4eb5574de3867932b76d542051f6f35ce714ee3e
Liam Cusack <LCusack8108@gmail.com>
2020-12-02 21:03:48 -0700

    Add all Restaurant user stories

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,45 @@
 class RestaurantsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def index
     @restaurants = Restaurant.all
   end
+
+  def new
+
+  end
+
+  def create
+    restaurant = Restaurant.new({
+                                name: params[:restaurant][:name],
+                                date_opened: params[:restaurant][:date_opened],
+                                style: params[:restaurant][:style]
+                                })
+      restaurant.save
+      redirect_to '/restaurants'
+    end
+
+    def show
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def edit
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    def update
+      restaurant = Restaurant.find(params[:id])
+      restaurant.update({
+                        name: params[:restaurant][:name],
+                        date_opened: params[:restaurant][:date_opened],
+                        style: params[:restaurant][:style]
+                        })
+        restaurant.save
+        redirect_to '/restaurants/#{restaurant.id}'
+    end
+
+    def destroy
+      Restaurant.destroy(params[:id])
+      redirect_to '/restaurants'
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   root 'welcome#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/networks', to: 'networks#index'
-  get '/restaurants', to: 'restaurants#index'
   get '/networks/new', to: 'networks#new'
   post '/networks', to: 'networks#create'
   get '/networks/:id', to: 'networks#show'
   get '/networks/:id/edit', to: 'networks#edit'
   patch '/networks/:id', to: 'networks#update'
   delete '/networks/:id', to: 'networks#destroy'
+
+  get '/restaurants', to: 'restaurants#index'
+  get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
+  get '/restaurants/:id', to: 'restaurants#show'
+  get '/restaurants/:id/edit', to: 'restaurants#edit'
+  patch '/restaurants/:id', to: 'restaurants#update'
+  delete '/restaurants/:id', to: 'restaurants#destroy'
 end
