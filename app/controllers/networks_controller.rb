35eb6d05786cd93f96e94ba52e817cf83e7a6ebe
Cydnee Owens <cks410@yahoo.com>
2020-12-02 18:19:18 -0700

    add new network button

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -3,13 +3,24 @@ class NetworksController < ApplicationController
     @networks = Network.all
   end
 
+  def new
+    
+  end
+
   def create
+    require 'pry'; binding.pry
     network = Network.new({
                            name: params[:network][:name],
                            established: params[:network][:established],
                            num_of_shows: params[:network][:num_of_shows]
                         })
+    require 'pry'; binding.pry
     network.save
+    require 'pry'; binding.pry
     redirect_to '/networks'
+  end
+
+  def show
+    @network = Network.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/networks', to: 'networks#index'
   get '/restaurants', to: 'restaurants#index'
+  post '/networks', to: 'networks#create'
+  get '/networks/:id', to: 'networks#show'
+  get '/networks/new', to: 'networks#new'
 end
