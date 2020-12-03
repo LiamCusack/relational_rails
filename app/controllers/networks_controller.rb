0c29a5cbedfaf9d19881c3080012c9226205fd1e
Cydnee Owens <cks410@yahoo.com>
2020-12-02 17:25:45 -0700

    add create method to network controller

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -2,4 +2,14 @@ class NetworksController < ApplicationController
   def index
     @networks = Network.all
   end
+
+  def create
+    network = Network.new({
+                           name: params[:network][:name],
+                           established: params[:network][:established],
+                           num_of_shows: params[:network][:num_of_shows]
+                        })
+    network.save
+    redirect_to '/networks'
+  end
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,3 @@
 class Network < ApplicationRecord
-  
+
 end
