641045f76985e3768769c559681839ea34bba839
Cydnee Owens <cks410@yahoo.com>
2020-12-04 11:19:16 -0700

    update to form tag

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -9,10 +9,15 @@ class NetworksController < ApplicationController
   end
 
   def create
-     network = Network.new({
-                            name: params[:network][:name],
-                            established: params[:network][:established],
-                            num_of_shows: params[:network][:num_of_shows]
+    #  network = Network.new({
+    #                         name: params[:network][:name],
+    #                         established: params[:network][:established],
+    #                         num_of_shows: params[:network][:num_of_shows]
+    #                       })
+    network = Network.new({
+                            name: params[:name],
+                            established: params[:established],
+                            num_of_shows: params[:num_of_shows]
                           })
     network.save
     redirect_to '/networks'
